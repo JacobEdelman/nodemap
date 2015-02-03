3f5ae87148bb0c333302e5ec90bfd97f6d1fba78
Jacob Edelman <edelman.jd@gmail.com>
2015-02-03 13:23:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ if(url.slice(-1) != '/') {
 
 var url_array = [];
 
-var map = function(loc) {
-
+var map = function(loc,depth) {
+	if(depth == undefined) depth = 0;
+	if(depth > 5) return;
 	//taking out GET arguments
 	var loc_no_arg = loc.split('?')[0];
-	
+
 	r(loc_no_arg, function(err, resp, body) {
 		if (err) {
 			console.log("down");
@@ -28,35 +29,27 @@ var map = function(loc) {
 			var not_duplicate;
 			var next_loc;
 			var next_loc_obj;
-			f.exec(
-				function() { target_loc = $(this).attr('href'); },
-				function(err) { if (err) throw err; console.log(target_loc); console.log("aasdasddddddd"); },
-				function(err) { if (err) throw err; console.log("asd"); not_duplicate = true; },
-				function(err) { if (err) throw err; next_loc_obj = u.parse(target_loc, false, true); },
-				function(err) {
-					if (err) throw err;
+			target_loc = $(this).attr('href');
+			if (target_loc == undefined) return;
+			console.log(target_loc)
+			if(target_loc.indexOf(".") == -1 )return;
+				not_duplicate = true;
+				next_loc_obj = u.parse(target_loc, false, true);
 					if(next_loc_obj.hostname == null) {
 						next_loc = u.resolve(loc_no_arg, target_loc);
-						console.log("asdsad"+loc_no_arg+"asd");
 					} else {
 						not_duplicate = false;
 					}
-				},
-				function(err) {
-					if (err) throw err;
 					for(var i=0; i<url_array.length; i++) {
 						if(url_array[i]==next_loc) {
 							not_duplicate = false;
 						}
 					}
-				},
-				function(err) {
 					if (err) throw err;
 					if(not_duplicate) {
-						url_array[url_array.length] = next_loc;
+						url_array.push(next_loc);
+						map(next_loc,depth+1)
 					}
-				}
-			);
 		});
 	});
 };
